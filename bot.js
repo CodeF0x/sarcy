a29f6ca26f74f2fe5ef5742a3968a5fa22f59647
toettl <toettl@united-internet.de>
2020-01-29 14:16:15 +0100

    Formatting.

--- bot.js
+++ bot.js
@@ -4,70 +4,64 @@ const uuid1 = require('uuid/v1');
 bot = new telegraf(process.env.TOKEN);
 
 bot.start(ctx => {
-    const message = 'Hello. Use /help for help.';
-    ctx.reply(message);
+  const message = 'Hello. Use /help for help.';
+  ctx.reply(message);
 });
 
 bot.help(ctx => {
-    const message = `
+  const message = `
     Write me something and I'll send it back to you but in sArCaSm font, so you can copy and paste it.
 
     Or
 
     Just type "@sarcybot I want this in sarcasm font" in another chat and I'll do all the things for you.
     `;
-    ctx.reply(message);
+  ctx.reply(message);
 });
 
 bot.on('message', ctx => {
-    if (ctx.message === '/start' || ctx.message === '/help') {
-        return;
-    }
+  if (ctx.message === '/start' || ctx.message === '/help') {
+    return;
+  }
 
-    const sarcasm = makeSarcasm(ctx.message);
-    ctx.reply(sarcasm);
+  const sarcasm = makeSarcasm(ctx.message);
+  ctx.reply(sarcasm);
 });
 
-bot.on('inline_query', ({
-    inlineQuery,
-    answerInlineQuery
-}) => {
-    const sarcasmString = makeSarcasm(inlineQuery.query);
+bot.on('inline_query', ({ inlineQuery, answerInlineQuery }) => {
+  const sarcasmString = makeSarcasm(inlineQuery.query);
 
-    if (!sarcasmString) {
-      return;
+  if (!sarcasmString) {
+    return;
+  }
+
+  const option = {
+    type: 'article',
+    id: uuid1(),
+    title: sarcasmString,
+    input_message_content: {
+      message_text: sarcasmString
     }
+  };
 
-    const option = {
-      type: 'article',
-      id: uuid1(),
-      title: sarcasmString,
-      input_message_content: {
-        message_text: sarcasmString
-      }
-    };
-
-    return answerInlineQuery([option]);
+  return answerInlineQuery([option]);
 });
 
-bot.on('choosen_inline_result', ({
-    chosenInlineResult
-}) => {
-    console.log(chosenInlineResult);
+bot.on('choosen_inline_result', ({ chosenInlineResult }) => {
+  console.log(chosenInlineResult);
 });
 
 bot.launch();
 
-
 function makeSarcasm(string) {
-    const sarcasm = [];
+  const sarcasm = [];
 
-    for (let i = 0; i < string.length; i++) {
-        if (i % 2 === 0) {
-            sarcasm.push(string[i].toUpperCase());
-        } else {
-            sarcasm.push(string[i].toLowerCase());
-        }
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      sarcasm.push(string[i].toUpperCase());
+    } else {
+      sarcasm.push(string[i].toLowerCase());
     }
-    return sarcasm.join('');
+  }
+  return sarcasm.join('');
 }
