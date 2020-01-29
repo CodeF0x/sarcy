d26cb93a57a87b089279ea7e2bc7d75a86874ca5
CodeF0x <contact@codef0x.dev>
2020-01-29 10:45:39 +0100

    Bot working.

---
+++ bot.js
@@ -0,0 +1,48 @@
+const telegraf = require('telegraf');
+const uuid1 = require('uuid/v1');
+
+bot = new telegraf('1068551164:AAGWO-GsNXZLJ9JTAnFh_fiVyxXM3-31sSI');
+
+bot.on('inline_query', ({
+    inlineQuery,
+    answerInlineQuery
+}) => {
+    const sarcasmString = makeSarcasm(inlineQuery.query);
+
+    if (!sarcasmString) {
+      return;
+    }
+
+    const option = {
+      type: 'article',
+      id: uuid1(),
+      title: sarcasmString,
+      input_message_content: {
+        message_text: sarcasmString
+      }
+    };
+
+    return answerInlineQuery([option]);
+});
+
+bot.on('choosen_inline_result', ({
+    chosenInlineResult
+}) => {
+    console.log(chosenInlineResult);
+});
+
+bot.launch();
+
+
+function makeSarcasm(string) {
+    const sarcasm = [];
+
+    for (let i = 0; i < string.length; i++) {
+        if (i % 2 === 0) {
+            sarcasm.push(string[i].toUpperCase());
+        } else {
+            sarcasm.push(string[i].toLowerCase());
+        }
+    }
+    return sarcasm.join('');
+}
